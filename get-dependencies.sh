654d9661586bcfa7f271580b350b949214d9799f
pflyly <81011361+pflyly@users.noreply.github.com>
2025-03-28 11:21:17 +0100

    maybe this?

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,7 +12,6 @@ fi
 
 LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
 FFMPEG_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/ffmpeg-mini-$PKG_TYPE"
-QT6_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/qt6-base-iculess-$PKG_TYPE"
 LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
 
 echo "Installing build dependencies..."
@@ -98,18 +97,15 @@ fi
 echo "Installing debloated pckages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$LLVM_URL" -O ./llvm-libs.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$QT6_URL" -O ./qt6-base-iculess.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$LIBXML_URL" -O ./libxml2-iculess.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$FFMPEG_URL" -O ./ffmpeg-mini-x86_64.pkg.tar.zst
 
 pacman -U --noconfirm \
-	./qt6-base-iculess.pkg.tar.zst \
 	./libxml2-iculess.pkg.tar.zst \
 	./ffmpeg-mini-x86_64.pkg.tar.zst \
 	./llvm-libs.pkg.tar.zst
 
-rm -f ./qt6-base-iculess.pkg.tar.zst \
-	./libxml2-iculess.pkg.tar.zst \
+rm -f .	./libxml2-iculess.pkg.tar.zst \
 	./ffmpeg-mini-x86_64.pkg.tar.zst \
 	./llvm-libs.pkg.tar.zst
 
