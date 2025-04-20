2e9ce95d0a3332b7712ad5f3d029efd27b6510e9
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-20 20:08:56 +0100

    matrix build

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -6,7 +6,27 @@ export APPIMAGE_EXTRACT_AND_RUN=1
 export ARCH="$(uname -m)"
 
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
-ARCH_FLAGS="-march=znver2 -mtune=znver2 -O3 -pipe -fno-plt -flto=auto -Wno-error"
+
+if [ "$1" = 'steamdeck' ]; then
+	echo "Making Citron Optimized Build for Steam Deck "
+	ARCH_FLAGS="-march=znver2 -mtune=znver2"
+ 	TARGET="Steamdeck"
+fi
+
+if [ "$1" = 'rog' ]; then
+	echo "Making Citron Optimized Build for ROG Ally X "
+	ARCH_FLAGS="-march=znver4 -mtune=znver4"
+ 	TARGET="ROG_Ally_X"
+fi
+
+if [ "$1" = 'common' ]; then
+	echo "Making Citron Optimized Build for Modern CPUs "
+	ARCH_FLAGS="-march=x86-64-v3 -mtune=x86-64-v3"
+ 	ARCH="${ARCH}_v3"
+  	TARGET="Common"
+fi
+
+EXTRA_FLAGS="-O3 -pipe -fno-plt -flto=auto -Wno-error"
 UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 
 # BUILD CITRON, fallback to mirror if upstream repo fails to clone
@@ -20,11 +40,9 @@ cd ./citron
 COUNT="$(git rev-list --count HEAD)"
 HASH="$(git rev-parse --short HEAD)"
 DATE="$(date +"%Y%m%d")"
-echo "Making nightly build"
-
 git submodule update --init --recursive -j$(nproc)
 
-#Replaces 'boost::asio::io_service' with 'boost::asio::io_context' for compatibility with Boost.ASIO versions 1.74.0 and later
+# Replaces 'boost::asio::io_service' with 'boost::asio::io_context' for compatibility with Boost.ASIO versions 1.74.0 and later
 find src -type f -name '*.cpp' -exec sed -i 's/boost::asio::io_service/boost::asio::io_context/g' {} \;
 
 mkdir build
@@ -41,42 +59,36 @@ cmake .. -GNinja \
  	-DSDL_PIPEWIRE=OFF \
    	-DBUNDLE_SPEEX=ON \
     	-DCMAKE_INSTALL_PREFIX=/usr \
-     	-DCMAKE_CXX_FLAGS="$ARCH_FLAGS -mfpmath=both" \
-      	-DCMAKE_C_FLAGS="$ARCH_FLAGS" \
+     	-DCMAKE_CXX_FLAGS="$ARCH_FLAGS $EXTRA_FLAGS -mfpmath=both" \
+      	-DCMAKE_C_FLAGS="$ARCH_FLAGS $EXTRA_FLAGS" \
        	-DCMAKE_EXE_LINKER_FLAGS="-Wl,-O3 -Wl,--as-needed" \
 	-DCMAKE_SYSTEM_PROCESSOR="$(uname -m)" \
  	-DCMAKE_BUILD_TYPE=Release \
   	-DCMAKE_POLICY_VERSION_MINIMUM=3.5
 ninja
-sudo ninja install
 echo "$HASH" >~/hash
+echo "$(cat ~/hash)"
 
-# Make appimage using citron appimage-builder.sh, we only need it to generate the appdir
+# Use citron appimage-builder.sh to generate AppDir
 cd ..
 chmod +x ./appimage-builder.sh
 ./appimage-builder.sh citron ./build
 rm -rf ./build/deploy-linux/citron*.AppImage # Delete the generated appimage, cause it's useless now
-cp /usr/lib/libSDL3.so* ./build/deploy-linux/AppDir/usr/lib/ # Copying libsdl3 to the already done appdir
+cp /usr/lib/libSDL3.so* ./build/deploy-linux/AppDir/usr/lib/ # Copying libsdl3 to target AppDir
 
-# Turn appdir into appimage
+# Prepare uruntime
 cd ..
 wget -q "$URUNTIME" -O ./uruntime
 chmod +x ./uruntime
 
-# Keep the mount point (speeds up launch time)
-sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime
-
-#Add udpate info to runtime
+# Add udpate info to runtime
 echo "Adding update information \"$UPINFO\" to runtime..."
 ./uruntime --appimage-addupdinfo "$UPINFO"
 
+# Turn AppDir into appimage
 echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B32 \
-	--header uruntime \
-	-i ./citron/build/deploy-linux/AppDir -o Citron-nightly-"${DATE}"-"${COUNT}"-"${HASH}"-"$ARCH".AppImage
+./uruntime --appimage-mkdwarfs -f --set-owner 0 --set-group 0 --no-history --no-create-timestamp --compression zstd:level=22 -S26 -B32 \
+		--header uruntime -i ./citron/build/deploy-linux/AppDir -o Citron-nightly-"${DATE}"-"${COUNT}"-"${HASH}"-"${TARGET}"-"$ARCH".AppImage
 
 echo "Generating zsync file..."
 zsyncmake *.AppImage -u *.AppImage
