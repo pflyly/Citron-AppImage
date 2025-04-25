7a56c7f28e85009e419b351042cfa9e8f51bfb0d
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-25 23:11:58 +0100

    Update citron-android.sh

--- citron-android.sh
+++ citron-android.sh
@@ -2,11 +2,6 @@
 
 git clone 'https://git.citron-emu.org/Citron/Citron.git' ./citron
 
-if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
-    export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
-    base64 --decode <<< "${ANDROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
-fi
-
 cd ./citron
 git submodule update --init --recursive
 find src -type f -name 'build.gradle.kts' -exec sed -i 's/"4\.0\.1"/"3.31.6"/g' {} +
@@ -19,9 +14,6 @@ cd src/android
 chmod +x ./gradlew
 ./gradlew assembleRelease --console=plain --info
 
-if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
-    rm "${ANDROID_KEYSTORE_FILE}"
-fi
 APK_PATH=$(find app/build/outputs/apk -type f -name "*.apk" | head -n 1)
 if [ -z "$APK_PATH" ]; then
     echo "Error: APK not found in expected directory."
