08e19871efa12e14abbd092cdd795f73007fe370
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-25 19:36:45 +0100

    Update citron-android.sh

--- citron-android.sh
+++ citron-android.sh
@@ -2,11 +2,7 @@
 
 export NDK_CCACHE=$(which ccache)
 
-if ! git clone 'https://git.citron-emu.org/Citron/Citron.git' ./citron; then
-    echo "Using mirror instead..."
-    rm -rf ./citron || true
-    git clone 'https://github.com/pkgforge-community/git.citron-emu.org-Citron-Citron.git' ./citron
-fi
+git clone 'https://github.com/pkgforge-community/git.citron-emu.org-Citron-Citron.git' ./citron
 
 if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
     export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
