9d1e1638b0185571898136abee376d959ef2f043
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-25 15:03:17 +0100

    initial

--- citron-android.sh
+++ citron-android.sh
@@ -8,19 +8,27 @@ if ! git clone 'https://git.citron-emu.org/Citron/Citron.git' ./citron; then
     git clone 'https://github.com/pkgforge-community/git.citron-emu.org-Citron-Citron.git' ./citron
 fi
 
-
 if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
     export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
     base64 --decode <<< "${ANDROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
 fi
 
-cd ./citron/src/android
+cd ./citron
+COUNT="$(git rev-list --count HEAD)"
+HASH="$(git rev-parse --short HEAD)"
+DATE="$(date +"%Y%m%d")"
+APK_NAME="Citron-nightly-"${DATE}"-"${COUNT}"-"${HASH}"-android-universal
+git submodule update --init --recursive -j$(nproc)
+
+cd src/android
 chmod +x ./gradlew
 ./gradlew assembleRelease
 ./gradlew bundleRelease
-
 ccache -s -v
 
 if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
     rm "${ANDROID_KEYSTORE_FILE}"
 fi
+
+mkdir -p artifacts
+mv build/bundle/app-release.apk "artifacts/$APK_NAME.apk"
