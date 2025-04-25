55fd449659a6152dd13c1cb6821ca97447813af9
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-25 12:08:36 +0100

    Update citron-appimage.sh

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -10,24 +10,33 @@ URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime
 case "$1" in
     steamdeck)
         echo "Making Citron Optimized Build for Steam Deck"
-        OPTIMIZE_FLAGS="-DCITRON_ENABLE_LTO=ON -DCMAKE_EXE_LINKER_FLAGS=-Wl,-O3\ -Wl,--as-needed -DCMAKE_CXX_FLAGS=-march=znver2\ -mtune=znver2\ -O3\ -pipe\ -fno-plt\ -flto=auto\ -Wno-error\ -mfpmath=both -DCMAKE_C_FLAGS=-march=znver2\ -mtune=znver2\ -O3\ -pipe\ -fno-plt\ -flto=auto\ -Wno-error"
+        CMAKE_EXE_LINKER_FLAGS="-Wl,-O3 -Wl,--as-needed"
+        CMAKE_CXX_FLAGS="-march=znver2 -mtune=znver2 -O3 -pipe -fno-plt -flto=auto -Wno-error -mfpmath=both"
+        CMAKE_C_FLAGS="-march=znver2 -mtune=znver2 -O3 -pipe -fno-plt -flto=auto -Wno-error"
+        CITRON_ENABLE_LTO=ON
         TARGET="Steamdeck"
         ;;
     rog)
         echo "Making Citron Optimized Build for ROG Ally X"
-        OPTIMIZE_FLAGS="-DCITRON_ENABLE_LTO=ON -DCMAKE_EXE_LINKER_FLAGS=-Wl,-O3\ -Wl,--as-needed -DCMAKE_CXX_FLAGS=-march=znver4\ -mtune=znver4\ -O3\ -pipe\ -fno-plt\ -flto=auto\ -Wno-error\ -mfpmath=both -DCMAKE_C_FLAGS=-march=znver4\ -mtune=znver4\ -O3\ -pipe\ -fno-plt\ -flto=auto\ -Wno-error"
+        CMAKE_EXE_LINKER_FLAGS="-Wl,-O3 -Wl,--as-needed"
+        CMAKE_CXX_FLAGS="-march=znver4 -mtune=znver4 -O3 -pipe -fno-plt -flto=auto -Wno-error -mfpmath=both"
+        CMAKE_C_FLAGS="-march=znver4 -mtune=znver4 -O3 -pipe -fno-plt -flto=auto -Wno-error"
+        CITRON_ENABLE_LTO=ON
         TARGET="ROG_Ally_X"
         ;;
     common)
         echo "Making Citron Optimized Build for Modern CPUs"
-        OPTIMIZE_FLAGS="-DCITRON_ENABLE_LTO=ON -DCMAKE_EXE_LINKER_FLAGS=-Wl,-O3\ -Wl,--as-needed -DCMAKE_CXX_FLAGS=-march=x86-64-v3\ -O3\ -pipe\ -fno-plt\ -flto=auto\ -Wno-error\ -mfpmath=both -DCMAKE_C_FLAGS=-march=x86-64-v3\ -O3\ -pipe\ -fno-plt\ -flto=auto\ -Wno-error"
+        CMAKE_EXE_LINKER_FLAGS="-Wl,-O3 -Wl,--as-needed"
+        CMAKE_CXX_FLAGS="-march=x86-64-v3 -O3 -pipe -fno-plt -flto=auto -Wno-error -mfpmath=both"
+        CMAKE_C_FLAGS="-march=x86-64-v3 -O3 -pipe -fno-plt -flto=auto -Wno-error"
+        CITRON_ENABLE_LTO=ON
         ARCH="${ARCH}_v3"
         TARGET="Common"
         ;;
     check)
         echo "Checking build"
-        OPTIMIZE_FLAGS="-DCITRON_USE_PRECOMPILED_HEADERS=OFF"
-        TARGET="Checck"
+        CITRON_USE_PRECOMPILED_HEADERS=OFF
+        TARGET="Check"
         CCACHE="ccache"
         ;;
 esac
@@ -36,9 +45,9 @@ UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARC
 
 # BUILD CITRON, fallback to mirror if upstream repo fails to clone
 if ! git clone 'https://git.citron-emu.org/Citron/Citron.git' ./citron; then
-	echo "Using mirror instead..."
-	rm -rf ./citron || true
-	git clone 'https://github.com/pkgforge-community/git.citron-emu.org-Citron-Citron.git' ./citron
+    echo "Using mirror instead..."
+    rm -rf ./citron || true
+    git clone 'https://github.com/pkgforge-community/git.citron-emu.org-Citron-Citron.git' ./citron
 fi
 
 cd ./citron
@@ -50,24 +59,28 @@ git submodule update --init --recursive -j$(nproc)
 mkdir build
 cd build
 cmake .. -GNinja \
-	-DCITRON_USE_BUNDLED_VCPKG=ON \
- 	-DCITRON_USE_BUNDLED_QT=OFF \
-  	-DUSE_SYSTEM_QT=ON \
- 	-DCITRON_TESTS=OFF \
-  	-DCITRON_CHECK_SUBMODULES=OFF \
-	-DCITRON_USE_LLVM_DEMANGLE=OFF \
-  	-DCITRON_USE_FASTER_LD=ON \
-   	-DENABLE_QT_TRANSLATION=ON \
-	-DUSE_DISCORD_PRESENCE=OFF \
- 	-DSDL_PIPEWIRE=OFF \
-   	-DBUNDLE_SPEEX=ON \
-	-DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    	-DCMAKE_INSTALL_PREFIX=/usr \
-	-DCMAKE_SYSTEM_PROCESSOR=x86_64 \
- 	-DCMAKE_BUILD_TYPE=Release \
-  	-DCMAKE_C_COMPILER_LAUNCHER="$CCACHE" \
-   	-DCMAKE_CXX_COMPILER_LAUNCHER="$CCACHE" \
-    	"$OPTIMIZE_FLAGS"
+    -DCITRON_USE_BUNDLED_VCPKG=ON \
+    -DCITRON_USE_BUNDLED_QT=OFF \
+    -DUSE_SYSTEM_QT=ON \
+    -DCITRON_TESTS=OFF \
+    -DCITRON_CHECK_SUBMODULES=OFF \
+    -DCITRON_USE_LLVM_DEMANGLE=OFF \
+    -DCITRON_USE_FASTER_LD=ON \
+    -DENABLE_QT_TRANSLATION=ON \
+    -DUSE_DISCORD_PRESENCE=OFF \
+    -DSDL_PIPEWIRE=OFF \
+    -DBUNDLE_SPEEX=ON \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER_LAUNCHER="${CCACHE:-}" \
+    -DCMAKE_CXX_COMPILER_LAUNCHER="${CCACHE:-}" \
+    ${CITRON_ENABLE_LTO:+-DCITRON_ENABLE_LTO=$CITRON_ENABLE_LTO} \
+    ${CITRON_USE_PRECOMPILED_HEADERS:+-DCITRON_USE_PRECOMPILED_HEADERS=$CITRON_USE_PRECOMPILED_HEADERS} \
+    ${CMAKE_EXE_LINKER_FLAGS:+-DCMAKE_EXE_LINKER_FLAGS="$CMAKE_EXE_LINKER_FLAGS"} \
+    ${CMAKE_CXX_FLAGS:+-DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS"} \
+    ${CMAKE_C_FLAGS:+-DCMAKE_C_FLAGS="$CMAKE_C_FLAGS"}
 ninja -j$(nproc)
 echo "$HASH" >~/hash
 echo "$(cat ~/hash)"
