e07e75a26beb4d4b9e9d735473dd2a7cd501bc00
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-26 00:29:01 +0100

    added Android release

---
+++ citron-android.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+git clone 'https://git.citron-emu.org/Citron/Citron.git' ./citron
+
+cd ./citron
+git submodule update --init --recursive
+find src -type f -name 'build.gradle.kts' -exec sed -i 's/"4\.0\.1"/"3.31.6"/g' {} +
+COUNT="$(git rev-list --count HEAD)"
+HASH="$(git rev-parse --short HEAD)"
+DATE="$(date +"%Y%m%d")"
+APK_NAME="Citron-nightly-${DATE}-${COUNT}-${HASH}-android-universal"
+
+cd src/android
+chmod +x ./gradlew
+./gradlew assembleRelease --console=plain --info
+
+APK_PATH=$(find app/build/outputs/apk -type f -name "*.apk" | head -n 1)
+if [ -z "$APK_PATH" ]; then
+    echo "Error: APK not found in expected directory."
+    exit 1
+fi
+mkdir -p artifacts
+mv "$APK_PATH" "artifacts/$APK_NAME.apk"
