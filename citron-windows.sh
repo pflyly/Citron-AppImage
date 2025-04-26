5009a5d07188ac24e3f959d3ac3ae9ec48dc3979
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-26 18:23:49 +0100

    save

--- citron-windows.sh
+++ citron-windows.sh
@@ -8,55 +8,40 @@ git submodule update --init --recursive
 COUNT="$(git rev-list --count HEAD)"
 HASH="$(git rev-parse --short HEAD)"
 DATE="$(date +"%Y%m%d")"
-EXE_NAME="Citron-nightly-${DATE}-${COUNT}-${HASH}-android-universal"
 
 case "$1" in
-    msvc_optimize)
-        echo "Making Citron Optimized Build for Windows"
-        CMAKE_EXE_LINKER_FLAGS="-Wl,-O3 -Wl,--as-needed"
-        CMAKE_CXX_FLAGS="-march=znver2 -mtune=znver2 -O3 -pipe -fno-plt -flto=auto -Wno-error -mfpmath=both"
-        CMAKE_C_FLAGS="-march=znver2 -mtune=znver2 -O3 -pipe -fno-plt -flto=auto -Wno-error"
-        CITRON_ENABLE_LTO=ON
-        TARGET="Windows(MSVC)"
+    msvc)
+        echo "Making Citron for Windows (MSVC)"
+        TARGET="Windows_MSVC"
         ;;
-    msvc_check)
-        echo "Checking build"
-        CITRON_USE_PRECOMPILED_HEADERS=OFF
-        TARGET="Check(MSVC)"
-        CCACHE="ccache"
+    msys2)
+        echo "Making Citron for Windows (MSYS2)"
+        TARGET="Windows_MSYS2"
         ;;
 esac
+EXE_NAME="Citron-nightly-${DATE}-${COUNT}-${HASH}-${TARGET}"
 
 mkdir build
 cd build
-cmake .. -GNinja \
-    -DCITRON_USE_BUNDLED_VCPKG=ON \
-    -DCITRON_USE_BUNDLED_QT=OFF \
-    -DUSE_SYSTEM_QT=ON \
+cmake .. -G Ninja \
     -DCITRON_TESTS=OFF \
     -DCITRON_CHECK_SUBMODULES=OFF \
-    -DCITRON_USE_LLVM_DEMANGLE=OFF \
-    -DCITRON_USE_FASTER_LD=ON \
+    -DCITRON_USE_PRECOMPILED_HEADERS=OFF \
     -DENABLE_QT_TRANSLATION=ON \
     -DUSE_DISCORD_PRESENCE=OFF \
-    -DSDL_PIPEWIRE=OFF \
-    -DBUNDLE_SPEEX=ON \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER_LAUNCHER="${CCACHE:-}" \
-    -DCMAKE_CXX_COMPILER_LAUNCHER="${CCACHE:-}" \
-    ${CITRON_ENABLE_LTO:+-DCITRON_ENABLE_LTO=$CITRON_ENABLE_LTO} \
-    ${CITRON_USE_PRECOMPILED_HEADERS:+-DCITRON_USE_PRECOMPILED_HEADERS=$CITRON_USE_PRECOMPILED_HEADERS} \
-    ${CMAKE_EXE_LINKER_FLAGS:+-DCMAKE_EXE_LINKER_FLAGS="$CMAKE_EXE_LINKER_FLAGS"} \
-    ${CMAKE_CXX_FLAGS:+-DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS"} \
-    ${CMAKE_C_FLAGS:+-DCMAKE_C_FLAGS="$CMAKE_C_FLAGS"}
-ninja -j$(nproc)
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+ninja
+ninja bundle
+strip -s bundle/*.exe
+ccache -s -v
 
-
-
-
+mkdir -p artifacts
+mkdir "$EXE_NAME"
+EXE_PATH=$(find ./ -type f -name "*.exe" | head -n 1)
+mv "$EXE_PATH"/* "$REV_NAME"
+ZIP_NAME="$REV_NAME"
 
 
 
