4e5beecab5631acee51b24ed29d1cb1ea265f15f
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-30 00:29:17 +0800

    added windows release again

--- citron-windows.sh
+++ citron-windows.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
+echo "Making Citron for Windows (MSVC)"
+export PATH="$PATH:/c/ProgramData/chocolatey/bin"
+
 if ! git clone 'https://git.citron-emu.org/citron/emu.git' ./citron; then
 	echo "Using mirror instead..."
 	rm -rf ./citron || true
@@ -12,25 +15,7 @@ git submodule update --init --recursive
 COUNT="$(git rev-list --count HEAD)"
 HASH="$(git rev-parse --short HEAD)"
 DATE="$(date +"%Y%m%d")"
-
-case "$1" in
-    msvc)
-        echo "Making Citron for Windows (MSVC)"
-        if ! echo "$PATH" | grep -q "/c/ProgramData/chocolatey/bin"; then
-            export PATH="$PATH:/c/ProgramData/chocolatey/bin"
-        fi
-        echo "PATH is: $PATH"
-        TARGET="Windows-MSVC"
-        ;;
-    msys2)
-        echo "Making Citron for Windows (MSYS2)"
-        echo "Patching bootstrap.sh to add shebang"
-        sed -i '1s;^;#!/usr/bin/bash\n;' externals/libusb/libusb/bootstrap.sh
-        chmod +x externals/libusb/libusb/bootstrap.sh
-        TARGET="Windows-MSYS2"
-        ;;
-esac
-EXE_NAME="Citron-nightly-${DATE}-${COUNT}-${HASH}-${TARGET}"
+EXE_NAME="Citron-nightly-${DATE}-${COUNT}-${HASH}-Windows-MSVC"
 
 mkdir build
 cd build
@@ -46,27 +31,17 @@ ninja
 EXE_PATH=./bin/citron.exe
 mkdir deploy
 cp -r bin/* deploy/
-windeployqt --release --no-compiler-runtime --no-opengl-sw --dir deploy "$EXE_PATH"
-rm deploy/Qt6*.pdb  # debug symbols aren't needed
+windeployqt --release --no-compiler-runtime --no-opengl-sw --no-system-d3d-compiler --dir deploy "$EXE_PATH"
 
-if [ "$1" = "msys2" ]; then
-    if command -v strip >/dev/null 2>&1; then
-            strip -s deploy/*.exe || true
-    fi        
-fi
+# Delete un-needed debug symbols 
+find deploy -type f -name "*.pdb" -exec rm -f {} +
 
 # Pack for upload
 mkdir -p artifacts
 mkdir "$EXE_NAME"
 cp -r deploy/* "$EXE_NAME"
 ZIP_NAME="$EXE_NAME.zip"
-
-if [ "$1" = "msvc" ]; then
-    powershell Compress-Archive "$EXE_NAME" "$ZIP_NAME"
-else
-    zip -r "$ZIP_NAME" "$EXE_NAME"
-fi
-
+powershell Compress-Archive "$EXE_NAME" "$ZIP_NAME"
 mv "$ZIP_NAME" artifacts/
 
 echo "Build completed successfully."
