ea1ab0e7ad5b3425be1ffb23df867c05aac8ba11
Escary <81011361+pflyly@users.noreply.github.com>
2025-05-01 17:19:59 +0800

    enable LTO for windows and mute check build error for linux

--- citron-appimage.sh
+++ citron-appimage.sh
@@ -36,6 +36,8 @@ case "$1" in
     check)
         echo "Checking build"
         CITRON_USE_PRECOMPILED_HEADERS=OFF
+	CMAKE_CXX_FLAGS="-w"
+        CMAKE_C_FLAGS="-w"
         TARGET="Check"
         CCACHE="ccache"
         ;;
